16f7ab646a83a39bdfd9cbc22033a2353b6e1698
ragil000 <ragilmanggalaning42@gmail.com>
2021-06-22 16:30:03 +0700

    tambah fungsi pengubah awalan nomor menjadi 62

--- app/controllers/senderController.js
+++ app/controllers/senderController.js
@@ -1,11 +1,16 @@
 const Sender = require('../models/senderModel')
 
+// fungsi untuk mengubah nomor awal menjadi 62
+String.prototype.IDNPhoneCode = function() {
+    return '62' + this.slice(1);
+}
+
 // fungsi untuk memanggil fungsi mengirim email
 exports.sender = async (result) => {
     try {
         for(let i=0; i<result.receivers.length; i++) {
             let newData = {
-                "receiver": result.receivers[i],
+                "receiver": result.receivers[i].IDNPhoneCode(),
                 "text": result.message
             }
 
